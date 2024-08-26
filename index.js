d43552e5da267b5380f0cefd795fa035fe84c2a0
adam123425343 <adamkhiami11@gmail.com>
2024-08-26 09:11:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,50 @@ client.on('message', msg => {
     }
     else {
         msg.reply(msg.content);
+      import os
+import discord
+import random
+from discord.ext import commands
+import time
+
+#intentz
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event 
+async def on_ready():
+  print('welcome')
+
+
+
+#mines
+@bot.command()
+async def mines(ctx, roundid):
+    a = len(roundid)
+    if a == 36:
+
+      await ctx.reply(f'Checking {roundid}')
+
+
+      await anu(ctx, roundid)
+    else:
+      time.sleep(2)
+      await ctx.reply('Wrong Round Id')
+async def anu(ctx, roundid):
+  an = []
+  an.clear()
+  for i in range(5):
+    seq =  ["💣", "💣", "💣", "💣", "💣"]
+    a = random.randrange(0, len(seq))
+    seq[a] = "💎" 
+    an.append(" ".join(seq))
+  embed=discord.Embed(title="made by J_39102", description=f"")
+  embed.add_field(name="mines made by J_39102", value="\n".join(an), inline=False)
+  acc = str(random.randint(96, 97))
+  cashout = str(random.randint(1, 3))
+  embed.add_field(name="Accuracy", value=f"{acc}%")
+  embed.add_field(name="Safe Cash out", value=f"{cashout} Tiles")
+  await ctx.reply(ctx.author.mention, embed=embed)
     }
 });
 
